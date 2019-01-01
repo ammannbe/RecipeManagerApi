d3ce3ab5b26d2ecbcca16b430fb33723a6937bc4
AmmannBe <benjamin.ammann@gbssg.ch>
2019-01-01 21:09:03 +0100

    Fixed FormatHelper date/time errors

--- app/Helpers/FormatHelper.php
+++ app/Helpers/FormatHelper.php
@@ -4,15 +4,23 @@ namespace App\Helpers;
 
 class FormatHelper
 {
-    public static function date($date) {
-        $dateTime = new \DateTime($date);
-        return $dateTime->format('d.m.Y');
+    public static function date($date = NULL) {
+        if (!is_null($date)) {
+            $dateTime = new \DateTime($date);
+            return $dateTime->format('d.m.Y');
+        } else {
+            return '-';
+        }
     }
 
-    public static function time($time) {
-        $dateTime = new \DateTime($time);
-        $dateTime = $dateTime->format('H:i:s');
-        return $dateTime;
+    public static function time($time = NULL) {
+        if (!is_null($time)) {
+            $dateTime = new \DateTime($time);
+            $dateTime = $dateTime->format('H:i:s');
+            return $dateTime;
+        } else {
+            return '-';
+        }
     }
 
     public static function shorten($text, $length = 30, $marker = '...') {
