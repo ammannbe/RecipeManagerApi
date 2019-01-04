19d9bc26fd71cbac787d2f2fd84f19e146da7504
AmmannBe <benjamin.ammann@gbssg.ch>
2019-01-04 22:32:12 +0100

    changed styling

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -8,12 +8,9 @@
 @section('content')
 
     @php
-        $loggedIn = FALSE;
         $isRecipeOwner = FALSE;
 
         if (Auth::check()) {
-            $loggedIn = TRUE;
-
             if (Auth::user()->id == $recipe->user_id) {
                 $isRecipeOwner = TRUE;
             }
@@ -22,15 +19,17 @@
 
     <article class="manage">
         <ul>
-            @if ($loggedIn && $isRecipeOwner)
-                {{-- <li><a href="{{ url('/recipes/edit/'.$recipe->id) }}"><i class="pencil black"></i>Bearbeiten</a></li> --}}
-                <li>
-                    <a href="{{ url('/recipes/delete/'.$recipe->id) }}" class="confirmation">
-                        <i class="cross red"></i>
-                        Löschen
-                    </a>
-                </li>
-            @endif
+            @auth
+                @if ($isRecipeOwner)
+                    {{-- <li><a href="{{ url('/recipes/edit/'.$recipe->id) }}"><i class="pencil black"></i>Bearbeiten</a></li> --}}
+                    <li>
+                        <a href="{{ url('/recipes/delete/'.$recipe->id) }}" class="confirmation">
+                            <i class="cross red"></i>
+                            Löschen
+                        </a>
+                    </li>
+                @endif
+            @endauth
             {{-- <li><a href="{{ url('/recipes/print/'.$recipe->id) }}">Drucken</a></li> --}}
         </ul>
     </article>
@@ -61,7 +60,12 @@
     </article>
 
     <article class="ingredients">
-        <h2>Zutaten</h2>
+        <h2>
+            Zutaten
+            @auth
+                <a href="{{ url('/ingredients/add/' . $recipe->id) }}"><i class="plus-sign"></i></a>
+            @endauth
+        </h2>
         <ul>
             @foreach ($recipe->ingredientDetails as $ingredientDetail)
                 @php
@@ -77,15 +81,14 @@
                         }
                     @endphp
                     {{ $ingredientDetail->amount }} {{ $unit->name_shortcut }} {{ $ingredient->name }}{{ $prepText }}
-                    @if ($loggedIn && $isRecipeOwner)
-                        <a href="/ingredients/delete/{{ $ingredientDetail->id }}"><i class="cross red"></i></a>
-                    @endif
+                    @auth
+                        @if ($isRecipeOwner)
+                            <a href="/ingredients/delete/{{ $ingredientDetail->id }}"><i class="cross red big"></i></a>
+                        @endif
+                    @endauth
                 </li>
             @endforeach
         </ul>
-        @auth
-            <a href="{{ url('/ingredients/add/' . $recipe->id) }}"><i class="plus-sign"></i>Zutat hinzufügen</a>
-        @endauth
     </article>
 
     <article class="instructions">
@@ -100,36 +103,43 @@
     </article>
 
     <article class="ratings">
-        <h2>Bewertungen</h2>
-        @php
-            $class = '';
-        @endphp
-
-        @if ($loggedIn)
+        @auth
             @if (! \App\Rating::where('user_id', Auth::User()->id)
                 ->where('recipe_id', $recipe->id)
                 ->first())
-                    <a href="{{ url('/ratings/add/' . $recipe->id) }}"><i class="plus-sign"></i>Bewertung hinzufügen</a>
-                    <br><br>
+                    <h2>
+                        Bewertungen
+                        <a href="{{ url('/ratings/add/' . $recipe->id) }}"><i class="plus-sign"></i></a>
+                    </h2>
+            @else
+                <h2>Bewertungen</h2>
             @endif
-        @endif
+        @endauth
+
+        @php
+            $class = ''
+        @endphp
 
         @foreach ($recipe->ratings as $rating)
             @php
                 $user = \App\User::find($rating->user_id);
                 $ratingOwner = FALSE;
+            @endphp
 
-                if ($loggedIn) {
+            @auth
+                @php
                     if (\Auth::user()->id == $rating->user_id) {
                         $ratingOwner = TRUE;
                     }
-                }
-            @endphp
+                @endphp
+            @endauth
 
             <article class="{{ ($ratingOwner ? 'bg-grey' : '') }}">
-                @if ($loggedIn && $ratingOwner)
-                    <a href="{{ url('ratings/delete/'.$rating->id) }}" style="float:right;"><i class="cross red"></i>Löschen</a>
-                @endif
+                @auth
+                    @if ($ratingOwner)
+                        <a href="{{ url('ratings/delete/'.$rating->id) }}" class="delete"><i class="cross red big"></i></a>
+                    @endif
+                @endauth
                 <strong>
                     {{ $rating->criterion->name }}
                 </strong>
@@ -137,7 +147,7 @@
                     {{ $rating->comment }}
                 </div>
                 <div>
-                    <small>{{ FormatHelper::date($rating->created_at) }}, {{ $user->name }}</small>
+                    <small>{{ $user->name }}, {{ FormatHelper::date($rating->created_at) }}</small>
                 </div>
             </article>
         @endforeach
