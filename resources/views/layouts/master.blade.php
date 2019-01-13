be6a04d5daeb7fa0b501ca6caee337abcc17f5a2
benjamin <benjamin@narrenhaus.ch>
2019-01-13 12:03:35 +0100

    replaced logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <title>Cookbook - @yield('title', 'Narrenhaus')</title>
+
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Cookbook - @yield('title', 'Narrenhaus')</title>
+
+        <link rel="shortcut icon" href="/logo.png" type="image/x-icon">
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         <script
             src="https://code.jquery.com/jquery-3.3.1.min.js"
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
