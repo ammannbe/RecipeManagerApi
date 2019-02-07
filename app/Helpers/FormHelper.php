cb89eed14006cdd9dec6862261c723e0664200c0
benjamin <benjamin@narrenhaus.ch>
2019-02-07 19:24:53 +0100

    added FormHelper class

---
+++ app/Helpers/FormHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Helpers;
+
+class FormHelper
+{
+    public static function group(String $group) {
+        return '<div class="groups '.$group.'">';
+    }
+
+    public static function groups(Array $groups) {
+        $html = '';
+        foreach ($groups as $group) {
+            $html .= '<div class="groups '.$group.'">';
+        }
+        return $html;
+    }
+
+    public static function close(Int $number = 1) {
+        $html = '';
+        for ($i = 0; $i < $number; $i++) {
+            $html .= '</div>';
+        }
+        return $html;
+    }
+
+    public static function jsDropdown(Array $items) {
+        $html  = '<i class="arrow-down"></i>';
+        $html .= '<ul class="hidden js-dropdown">';
+        foreach ($items as $item) {
+            $html .= '<li>'.$item.'</li>';
+        }
+        $html .= '</ul>';
+        return $html;
+    }
+}
