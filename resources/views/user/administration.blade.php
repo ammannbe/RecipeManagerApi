60b9dc2af3c7202a4aeb0e4848f6496cbaceba7f
benjamin <benjamin@narrenhaus.ch>
2019-03-04 21:40:42 +0100

    corrected misspelling in admin page

--- resources/views/user/administration.blade.php
+++ resources/views/user/administration.blade.php
@@ -9,8 +9,8 @@
 
     <ul>
         <li><a href="{{ url('/cookbooks/create') }}">Kochbuch erstellen</a></li>
-        <li><a href="{{ url('/authors/create') }}">Author erstellen</a></li>
-        <li><a href="{{ url('/categories/create') }}">Kategories erstellen</a></li>
+        <li><a href="{{ url('/authors/create') }}">Autor erstellen</a></li>
+        <li><a href="{{ url('/categories/create') }}">Kategorie erstellen</a></li>
         <li><a href="{{ url('/ingredients/create') }}">Zutat erstellen</a></li>
         <li><a href="{{ url('/units/create') }}">Einheit erstellen</a></li>
         <li><a href="{{ url('/preps/create') }}">Zutaten-Vorbereitung erstellen</a></li>
