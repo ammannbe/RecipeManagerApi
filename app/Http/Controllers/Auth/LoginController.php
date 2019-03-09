8b906aa1158a04e554e94ab546a6768d2ef73d5b
benjamin <benjamin@narrenhaus.ch>
2019-03-09 18:26:29 +0100

    #47 Prefill new recipes with actual user name

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Author;
 use ReflectionClass;
 use Illuminate\Http\Request;
 use Adldap\Laravel\Facades\Adldap;
@@ -73,6 +74,8 @@ class LoginController extends Controller
             foreach ($sync_attrs as $field => $value) {
                 $user->$field = $value !== NULL ? $value : NULL;
             }
+
+            Author::firstOrCreate(['name' => $user->name]);
 
             $this->guard()->login($user, TRUE);
             return TRUE;
