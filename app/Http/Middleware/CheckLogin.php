4906a411ccd6ede26776efaf2d9e150bef888242
benjamin <benjamin@narrenhaus.ch>
2019-03-17 19:52:05 +0100

    Code style enhancement

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -16,7 +16,7 @@ class CheckLogin
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check()) {
+        if (auth()->check()) {
             return $next($request);
         } else {
             abort(401);
