89c076058e21e65a12f1038e1fc6f803cdb562ba
benjamin <benjamin@narrenhaus.ch>
2019-03-23 22:09:30 +0100

    Renamed App

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    <em>&copy; 2018 - {{ date('Y') }}</em> <a href="{{ Request::root() }}">Narrenhaus Cookbook</a>
+    <em>&copy; 2018 - {{ date('Y') }}</em> <a href="{{ Request::root() }}">{{ env('APP_NAME') }}</a>
 </footer>
