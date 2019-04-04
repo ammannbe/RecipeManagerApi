e87d139adc6d00162c9e108d0a84bd61f1a2c3d4
benjamin <benjamin@narrenhaus.ch>
2019-04-04 18:30:08 +0200

    added jquery locally

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,10 +24,7 @@
         @extends('layouts.navigation')          {{--  Main navigation --}}
         @extends('layouts.toast')               {{--  Toast messagess --}}
 
-        <script
-            src="https://code.jquery.com/jquery-3.3.1.min.js"
-            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-            crossorigin="anonymous"></script>
+        <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
         <script src="{{ mix('/js/app.js') }}"></script>
 
         <link rel="shortcut icon" href="/logo.png" type="image/x-icon">
