5ff53bb406fa7e949ec2b43a48f0565d5efd75b2
benjamin <benjamin@narrenhaus.ch>
2019-04-09 20:12:02 +0200

    removed navigation-right

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,5 +9,27 @@
         @auth
             <li><a href="{{ url('/recipes/create') }}">Rezept eingeben</a></li>
         @endauth
+        @guest
+            <li class="right">
+                <a href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+        @else
+            <li>
+                <a href="{{ url('/profile') }}">Profil</a>
+            </li>
+            @if (auth()->user()->user_type == 'admin')
+                <li class="desktop-hide right">
+                    <a href="{{ url('/admin') }}">Administration</a>
+                </li>
+            @endif
+            <li>
+                <div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <input type="submit" value="Abmelden">
+                    </form>
+                </div>
+            </li>
+        @endguest
     </ul>
 </nav>
