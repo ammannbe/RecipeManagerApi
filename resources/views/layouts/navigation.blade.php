242535c7790e96299158d2ea93a11f922ad0ec48
benjamin <benjamin@narrenhaus.ch>
2019-04-17 19:21:09 +0200

    simplified w3 navigation classes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,21 +1,23 @@
+@php($w3BarItemClasses = 'w3-bar-item w3-button w3-mobile w3-padding-large')
+
 <div class="w3-bar w3-black nav top">
-    <a href="{{ url('/') }}" class="w3-bar-item w3-button w3-mobile w3-padding-16"><i class="home"></i>Übersicht</a>
-    <a href="{{ url('/search') }}" class="w3-bar-item w3-button w3-mobile w3-padding-16"><i class="magnifier"></i>Suche</a>
+    <a href="{{ url('/') }}" class="{{ $w3BarItemClasses }}"><i class="home"></i>Übersicht</a>
+    <a href="{{ url('/search') }}" class="{{ $w3BarItemClasses }}"><i class="magnifier"></i>Suche</a>
     @auth
-        <a href="{{ url('/recipes/create') }}" class="w3-bar-item w3-button w3-mobile w3-padding-16">Rezept eingeben</a>
+        <a href="{{ url('/recipes/create') }}" class="{{ $w3BarItemClasses }}">Rezept eingeben</a>
     @endauth
 
 
     @guest
-        <a href="{{ route('login') }}" class="w3-bar-item w3-button w3-mobile w3-padding-16 w3-right w3-grey"><i></i>Login</a>
+        <a href="{{ route('login') }}" class="{{ $w3BarItemClasses }} w3-right w3-grey"><i></i>Login</a>
     @else
         <div class="w3-dropdown-hover w3-mobile w3-right">
-            <button class="w3-button w3-padding-16 dropdown-button">Account<i class="black-down-pointing-triangle"></i></button>
+            <button class="w3-button w3-padding-large dropdown-button">Account<i class="black-down-pointing-triangle"></i></button>
             <div class="w3-dropdown-content w3-bar-block w3-dark-grey">
-                <a href="{{ url('/profile') }}" class="w3-bar-item w3-button w3-mobile w3-padding-16">Profil</a>
+                <a href="{{ url('/profile') }}" class="{{ $w3BarItemClasses }}">Profil</a>
 
                 @if (auth()->user()->user_type == 'admin')
-                    <a href="{{ url('/admin') }}" class="w3-bar-item w3-button w3-mobile w3-padding-16">Administration</a>
+                    <a href="{{ url('/admin') }}" class="{{ $w3BarItemClasses }}">Administration</a>
                 @endif
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST">
