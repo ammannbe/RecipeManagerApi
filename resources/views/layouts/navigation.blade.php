f68fd488fa79bd559f5178cc1f0d1ccb645e9db7
benjamin <benjamin@narrenhaus.ch>
2019-05-04 17:23:17 +0200

    make navigation more mobile friendly

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,29 +1,55 @@
-@php($w3BarItemClasses = 'w3-bar-item w3-button w3-mobile w3-padding-large')
+@php($w3BarAlwaysItemClasses = 'w3-bar-item w3-button w3-padding-large')
+@php($w3BarLargeItemClasses  = 'w3-bar-item w3-button w3-hide-small w3-padding-large')
+@php($w3BarSmallItemClasses  = 'w3-bar-item w3-button w3-padding-large')
 
-<div class="w3-bar w3-black nav top">
-    <a href="{{ url('/') }}" class="{{ $w3BarItemClasses }}"><i class="home"></i>Rezepte</a>
-    <a href="{{ url('/search') }}" class="{{ $w3BarItemClasses }}"><i class="magnifier"></i>Suche</a>
-    @auth
-        <a href="{{ url('/recipes/create') }}" class="{{ $w3BarItemClasses }}">Rezept eingeben</a>
-    @endauth
-    @if (auth()->check() && auth()->user()->isAdmin())
-        <a href="{{ url('/admin') }}" class="{{ $w3BarItemClasses }}">Administration</a>
-    @endif
+<div class="nav top">
+    <div class="w3-bar w3-black">
+        <a href="{{ url('/') }}" class="{{ $w3BarAlwaysItemClasses }}"><i class="home"></i>Rezepte</a>
+        <a href="{{ url('/search') }}" class="{{ $w3BarAlwaysItemClasses }}"><i class="magnifier"></i>Suche</a>
+        @auth
+            <a href="{{ url('/recipes/create') }}" class="{{ $w3BarLargeItemClasses }}">Rezept eingeben</a>
+        @endauth
+        @if (auth()->check() && auth()->user()->isAdmin())
+            <a href="{{ url('/admin') }}" class="{{ $w3BarLargeItemClasses }}">Administration</a>
+        @endif
 
+        @guest
+            <a href="{{ route('login') }}" class="{{ $w3BarLargeItemClasses }} w3-right w3-grey"><i></i>Login</a>
+        @else
+            <a href="{{ url('/profile') }}" class="{{ $w3BarLargeItemClasses }} w3-right">Profil</a>
 
-    @guest
-        <a href="{{ route('login') }}" class="{{ $w3BarItemClasses }} w3-right w3-grey"><i></i>Login</a>
-    @else
-        <div class="w3-dropdown-hover w3-mobile w3-right">
-            <button class="w3-button w3-padding-large dropdown-button">Account<i class="black-down-pointing-triangle"></i></button>
-            <div class="w3-dropdown-content w3-bar-block w3-dark-grey">
-                <a href="{{ url('/profile') }}" class="{{ $w3BarItemClasses }}">Profil</a>
+            <form id="logout-form" action="{{ route('logout') }}" class="w3-hide-small w3-right" method="POST">
+                @csrf
+                <input type="submit" value="Abmelden" class="w3-button">
+            </form>
+        @endguest
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    <input type="submit" value="Abmelden" class="w3-button">
-                </form>
-            </div>
-        </div>
-    @endguest
+        <a href="javascript:void(0)" class="{{ $w3BarSmallItemClasses }} w3-right w3-hide-large w3-hide-medium" onclick="navigation()">&#9776;</a>
+    </div>
+
+    <div class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium mobile">
+        @auth
+            <a href="{{ url('/recipes/create') }}" class="{{ $w3BarSmallItemClasses }}">Rezept eingeben</a>
+        @endauth
+        @if (auth()->check() && auth()->user()->isAdmin())
+            <a href="{{ url('/admin') }}" class="{{ $w3BarSmallItemClasses }}">Administration</a>
+        @endif
+
+        @guest
+            <a href="{{ route('login') }}" class="{{ $w3BarSmallItemClasses }} w3-grey"><i></i>Login</a>
+        @else
+            <a href="{{ url('/profile') }}" class="{{ $w3BarSmallItemClasses }}">Profil</a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
+                @csrf
+                <input type="submit" value="Abmelden" class="w3-button">
+            </form>
+        @endguest
+    </div>
 </div>
+
+<script>
+    function navigation() {
+        $('.mobile').toggleClass('w3-show');
+    }
+</script>
