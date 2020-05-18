4f7ea29a404df9f611e8529ef03f32b302219c4e
benjamin <benjamin@narrenhaus.ch>
2020-05-18 21:27:49 +0200

    added small photo previews

--- resources/js/components/Recipes/RecipePhoto.vue
+++ resources/js/components/Recipes/RecipePhoto.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="photo">
-    <figure>
-      <img @click="openModal()" :src="photo_urls[0]" :alt="recipe.name" />
+    <figure class="preview">
+      <img
+        @click="openModal(photoUrls.indexOf(mainPhotoPreviewUrl))"
+        :src="mainPhotoPreviewUrl"
+        :alt="recipe.name"
+      />
     </figure>
+    <div v-if="photoUrls.length > 1" class="small-previews">
+      <figure :key="photoUrls.indexOf(photoUrl)" v-for="photoUrl in photoUrls">
+        <img @click="mainPhotoPreviewUrl = photoUrl" :src="photoUrl" :alt="recipe.name" />
+      </figure>
+    </div>
 
     <modal
-      v-if="recipe.photo_urls && show_photo !== false"
+      v-if="recipe.photo_urls && showPhoto !== false"
       :controls="true"
-      :disable-next="!photoExists(show_photo + 1)"
-      :disable-prev="show_photo <= 0"
+      :disable-next="!photoExists(showPhoto + 1)"
+      :disable-prev="showPhoto <= 0"
       @close="closeModal()"
       @next="next()"
       @prev="prev()"
     >
       <template>
         <figure>
-          <img :src="photo_urls[show_photo]" :alt="recipe.name" />
+          <img :src="photoUrls[showPhoto]" :alt="recipe.name" />
         </figure>
       </template>
     </modal>
@@ -29,14 +38,16 @@ export default {
   props: ["recipe"],
   data() {
     return {
-      show_photo: false,
-      photo_urls: ["https://bulma.io/images/placeholders/128x128.png"]
+      showPhoto: false,
+      photoUrls: ["https://bulma.io/images/placeholders/128x128.png"],
+      mainPhotoPreviewUrl: "https://bulma.io/images/placeholders/128x128.png"
     };
   },
   watch: {
     recipe() {
       if (this.recipe.photo_urls) {
-        this.photo_urls = this.recipe.photo_urls;
+        this.photoUrls = this.recipe.photo_urls;
+        this.mainPhotoPreviewUrl = this.photoUrls[0];
       }
     }
   },
@@ -49,17 +60,20 @@ export default {
         return;
       }
 
-      let show_photo = parseInt(this.$route.query.show_photo);
+      let showPhoto = parseInt(this.$route.query.show_photo);
 
-      if (!this.photoExists(show_photo)) {
+      if (!this.photoExists(showPhoto)) {
         this.closeModal();
         return;
       }
 
-      this.openModal(show_photo);
+      this.openModal(showPhoto);
     },
     openModal(index = 0) {
-      this.show_photo = index;
+      if (!this.recipe.photo_urls) {
+        return;
+      }
+      this.showPhoto = index;
 
       let query = RouteQueryHelper.pushOrReplace(
         this.$route.query,
@@ -69,25 +83,25 @@ export default {
       this.$router.push({ query });
     },
     closeModal() {
-      this.show_photo = false;
+      this.showPhoto = false;
 
       let query = RouteQueryHelper.remove(this.$route.query, "show_photo");
       this.$router.push({ query });
     },
     photoExists(index) {
-      return typeof this.photo_urls[index] !== "undefined";
+      return typeof this.photoUrls[index] !== "undefined";
     },
     next() {
-      if (!this.photoExists(this.show_photo + 1)) {
+      if (!this.photoExists(this.showPhoto + 1)) {
         return;
       }
-      this.openModal(this.show_photo + 1);
+      this.openModal(this.showPhoto + 1);
     },
     prev() {
-      if (!this.photoExists(this.show_photo - 1)) {
+      if (!this.photoExists(this.showPhoto - 1)) {
         return;
       }
-      this.openModal(this.show_photo - 1);
+      this.openModal(this.showPhoto - 1);
     }
   }
 };
@@ -96,14 +110,37 @@ export default {
 <style lang="scss" scoped>
 div.photo {
   display: flex;
+  flex-direction: column;
 
-  > figure {
+  > .preview {
     margin-right: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
     > img {
       max-width: 100%;
       max-height: 200px;
+      width: 100%;
+      object-fit: contain;
       cursor: zoom-in;
+    }
+  }
+
+  > .small-previews {
+    display: flex;
+    margin-right: 30px;
+
+    > figure {
+      width: 40px;
+      height: 40px;
+      margin: 5px 5px 0 0;
+      cursor: pointer;
+
+      > img {
+        height: 100%;
+        width: 100%;
+      }
     }
   }
 
