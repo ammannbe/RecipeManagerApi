fa3355eb37ce01d4b9ea77172cbf4951adf8aa5b
benjamin <benjamin@narrenhaus.ch>
2020-07-22 19:33:21 +0200

    removed orphaned JWT auth methods

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -106,24 +106,4 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasOne('\App\Models\Users\Author');
     }
-
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
