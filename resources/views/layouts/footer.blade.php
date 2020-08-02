a2833a95b34f83187d3216527c2389819d6d5659
benjamin <benjamin@narrenhaus.ch>
2020-08-02 19:18:00 +0200

    added footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>&#169; 2018 - {{ now()->format('Y') }} {{ config('app.name') }}</p>
+        <p><a href="https://github.com/ammannbe/cookbook/"><i class="fab fa-github"></i> GitHub</a></p>
+    </div>
+</footer>
